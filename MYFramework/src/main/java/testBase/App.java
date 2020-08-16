bf1348fd77c3c825e85e7491ff67039b196551c0
meetparag81 <paragborawake81@gmail.com>
2020-08-16 19:59:24 +0530

    Apptestclassmodified

--- MYFramework/src/main/java/testBase/App.java
+++ MYFramework/src/main/java/testBase/App.java
@@ -5,16 +5,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-/**
- * Hello world!
- *
- */
+import helper.Excelhelper.Exls_Reader;
+import helper.resorce.ResourceHelper;
+import pages.LoginPage; 
+
 public class App extends TestBase
 {
+	LoginPage LoginPage;
+	Exls_Reader reader = new Exls_Reader(ResourceHelper.GetResourcePath("\\java\\com_Milan_TestData\\Milandata.xlsx"));
+
 	@BeforeMethod
 	public void SetUp()
 	{
 		TestBase.initalization();
+		LoginPage = new LoginPage();
+		String username = reader.getCellData("Login", "Username", 2);
+		String password = reader.getCellData("Login", "Password", 2);
+		LoginPage.login(username, password);
 	}
   @Test(priority=0)
   public void AppTestForPassCondition(){
